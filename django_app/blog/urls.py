8197b9473725eabae14fc2172892e40fd0bd1f00
hanabee337 <hanabee337@gmail.com>
2017-03-26 17:39:12 +0900

    post_add : urls, view, template 기본 구현
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/blog/urls.py
+++ django_app/blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
 
     url(r'^detail/(?P<post_id>[0-9]+)/$', views.post_detail, name='post_detail'),
+
+    url(r'^add/$', views.post_add, name='post_add'),
 ]
--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -27,3 +27,12 @@ def post_detail(request, post_id):
     }
 
     return render(request, 'blog/post_detail.html', context)
+
+
+def post_add(request):
+    # return HttpResponse('post_add view')
+
+    context = {
+
+    }
+    return render(request, 'blog/post_add.html', context)
