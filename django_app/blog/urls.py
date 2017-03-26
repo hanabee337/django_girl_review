6974ab290d1534a0dcdbc0b4fb10ca216f7ee0a6
hanabee337 <hanabee337@gmail.com>
2017-03-26 16:39:57 +0900

    post_detail : url, view, template 작성
    context에서 'post-detail'이라고 설정하면, template에서 해석을 못함.
    'post_detail' 이라고 해야함.
    
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/blog/urls.py
+++ django_app/blog/urls.py
@@ -4,4 +4,6 @@ from blog import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+
+    url(r'^detail/(?P<post_id>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Post
@@ -16,3 +16,14 @@ def post_list(request):
         'posts': posts,
     }
     return render(request, 'blog/post_list.html', context)
+
+
+def post_detail(request, post_id):
+    # post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, id=post_id)
+
+    context = {
+        'post_detail': post,
+    }
+
+    return render(request, 'blog/post_detail.html', context)
