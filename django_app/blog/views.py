0bda4940c549b63b68d458d747841032360b0842
hanabee337 <hanabee337@gmail.com>
2017-03-26 15:31:39 +0900

    Django Template
    Signed-off-by: hanabee337 <hanabee337@gmail.com>

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,11 +1,18 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
 
 
 def post_list(request):
     # return HttpResponse('post_list view')
-    context = {
 
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now())
+
+    context = {
+        'posts': posts,
     }
     return render(request, 'blog/post_list.html', context)
